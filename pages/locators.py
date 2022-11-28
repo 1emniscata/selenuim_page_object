425bb5e21321c7356f28a5c2b533e0ad544dc32c
1emniscata <aleksandrsuzen@gmail.com>
2022-11-28 19:37:01 +0300

    Adds test to check 'add' btn

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,12 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.ID, "login_form")
     REGISTRATION_FORM = (By.ID, "register_form")
+
+
+class ProductPageLocators:
+    ADD_PRODUCT = (By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "div.col-sm-6.product_main>p.price_color")
+    PRODUCT_TITLE = (By.TAG_NAME, "h1")
+    BASKET_COST = (By.CSS_SELECTOR, "div.basket-mini.pull-right.hidden-xs")
+    ALERT_PRODUCT_TITLE = (By.CSS_SELECTOR, "div.alertinner>strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alertinner")
---
+++ pages/product_page.py
@@ -0,0 +1,29 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        add_btn = self.browser.find_element(*ProductPageLocators.ADD_PRODUCT)
+        add_btn.click()
+        # self.solve_quiz_and_get_code()
+        self.compare_titles()
+        self.compare_prices()
+
+    def compare_titles(self):
+        alert_product_title = self.browser.find_element(*ProductPageLocators.ALERT_PRODUCT_TITLE).text
+        product_title = self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE).text
+        assert alert_product_title == product_title, "Alert title differs from the product one"
+
+    def compare_prices(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        basket_price = self.browser.find_element(*ProductPageLocators.BASKET_COST).text.split(" ")[2]
+        basket_price = basket_price[:basket_price.index("\n")]
+        assert product_price == basket_price, "Basket price differs from the product one"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_not_be_element(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Element didn't disappear"
