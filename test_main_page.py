babe7cebbfe396ccae36f471244b5a24736ef18a
1emniscata <aleksandrsuzen@gmail.com>
2022-11-28 11:02:20 +0300

    Redefines the "test_guest_can_go_to_login_page" test

--- test_main_page.py
+++ test_main_page.py
@@ -1,20 +1,21 @@
-from selenium.webdriver.common.by import By
-
+from pages.login_page import LoginPage
 from .pages.main_page import MainPage
-
 
 # from pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
+
+
 # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-
 
 def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page(browser):
+    page = MainPage(browser, link)
+    page.open()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
